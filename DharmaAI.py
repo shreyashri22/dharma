2a926dbe241c7b3832bf588cbe6ae617a21f6478
shreyashri22 <shreya@pearlthoughts.com>
2023-09-25 18:00:17 +0530

    Update DharmaAI.py

--- DharmaAI.py
+++ DharmaAI.py
@@ -102,11 +102,10 @@ def main():
             if ('yes' or 'sure' or 'yeah') in tmp.lower():
                 res=consult()
                 st.write("Please select your preferable timeslot from below or visit https://calendly.com/shrivastavanolo/test to book a consultation")
-                for i in res:
                     # dt=datetime(i['scheduling_url'])
                     # date_string = dt.strftime("%B %d, %Y %H:%M")
                     # st.write(i['start_time'],date_string)    
-                    st.write(i)
+                st.write(res)
             tmp=None
 
 
